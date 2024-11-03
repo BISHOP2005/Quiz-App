cf5f4f4ab50e6dba3d20f64a2b419acfa9f26b8a
Prathamesh P Devalapurkar <142967619+BISHOP2005@users.noreply.github.com>
2024-11-03 10:37:20 +0530

    Add files via upload

---
+++ Quiz 2/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb:1,
+        question: "Which popular game launched the highly-anticipated sequel Marvel’s Spider-Man 2 in October 2024?",
+        answer: "C. Insomniac Games",
+        options: [
+            "A. Ubisoft",  
+            "B. Naughty Dog",  
+            "C. Insomniac Games",  
+            "D. Rockstar Games"  
+        ]
+    },
+    {
+        numb:2,
+        question: "Which social media platform recently introduced a feature allowing users to post voice notes as comments?",
+        answer: "B. X (formerly Twitter)",
+        options: [
+            "A. Instagram",
+            "B. X (formerly Twitter)",
+            "C. TikTok",
+            "D. Threads "
+        ]
+    },
+    {
+        numb:3,
+        question: "Which video game achieved a Guinness World Record for most concurrent players during its launch in 2024?",
+        answer: "A. Counter-Strike 2",
+        options: [
+            "A. Counter-Strike 2",
+            "B. Starfield",
+            "C. Baldur's Gate 3 ",
+            "D. Call of Duty: Modern Warfare III"
+        ]
+    },
+    {
+        numb:4,
+        question: " What is the name of the AI chatbot recently integrated into WhatsApp, Instagram, and Facebook Messenger?",
+        answer: "C. Meta AI",
+        options: [
+            "A. Bard",
+            "B. ChatGPT",
+            "C. Meta AI",
+            "D. Bing Chat"
+        ]
+    },
+    {
+        numb:5,
+        question: "Which game recently announced a collaboration with Lamborghini, offering virtual cars to players?",
+        answer: "B. Rocket League",
+        options: [
+            "A. Fortnite",
+            "B. Rocket League",
+            "C. PUBG Mobile",
+            "D. Gran Turismo 7"
+        ]
+    }   
+];
